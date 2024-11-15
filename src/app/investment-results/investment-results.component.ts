c9b11531b6236a3e135725b56e90e60a072008bf
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-11-15 16:59:06 +0100

    refactor: with signals

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ResultsData } from '../investment-input.model';
 import { CurrencyPipe } from '@angular/common';
 
@@ -10,5 +10,7 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() results?: ResultsData[]; // alternative with signals → results = input<ResultsData[]>()
+  // @Input() results?: ResultsData[];
+  // alternative with signals:
+  results = input<ResultsData[]>();
 }
