27978c7ca9e14e4d8c12c135d2e4c0ec494dc9dd
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2024-11-16 14:44:30 +0100

    refactor: add service component

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,6 @@
-import { Component, input } from '@angular/core';
-import { ResultsData } from '../investment-input.model';
+import { Component, inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -10,7 +10,9 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  // @Input() results?: ResultsData[];
-  // alternative with signals:
-  results = input<ResultsData[]>();
+  private investmentService = inject(InvestmentService);
+
+  get results() {
+    return this.investmentService.resultsData;
+  }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,17 +11,6 @@ import type { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  // @Output() calculate = new EventEmitter<InvestmentInput>();
-  // newer alternative:
-  calculate = output<InvestmentInput>();
-
-  // ---here is approach without signals
-  // enteredInitialInvestment = '0';
-  // enteredAnnualInvestment = '0';
-  // enteredExpectedReturn = '5';
-  // enteredDuration = '10';
-  // ---
-
   // ---with signals
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
@@ -28,8 +18,10 @@ export class UserInputComponent {
   enteredDuration = signal('10');
   // ---
 
+  constructor(private investmentService: InvestmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(), // alternative → Number(this.enteredAnnualInvestment)
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
